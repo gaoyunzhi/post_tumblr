94b365151240a97e1076ad2b312788dca6cb2f11
Yunzhi Gao <gaoyunzhi@gmail.com>
2023-01-21 09:48:08 -0800

    commit

--- post_tumblr.py
+++ post_tumblr.py
@@ -82,7 +82,7 @@ async def runImp():
             tumblr_user = credential['channels'][channel]['tumblr_user']
             result = await post_tumblr(tumblr_user, channel, post, album, status_text)
             if 'id' not in result:
-                print(result)
+                print(album.url, result)
             result_url = 'https://tumblr.com/%s/%d' % (tumblr_user, result['id'])
             # print(result_url)
             existing.update(album.url, result_url)
